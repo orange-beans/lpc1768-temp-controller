78800bd9c4983755b5bb7bb25b83ef1a0eb98a7f
orange-beans <cczhang@simtech.a-star.edu.sg>
2016-12-08 14:45:02 +0800

    Add PID controller class

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,31 @@
 #include <cJSON.h>
 #include <Flasher.h>
 #include <SawTooth.h>
+#include <PID.h>
+
 Serial pc(USBTX, USBRX);
 
+//****************************************************************************/
+// Defines PID parameters
+//****************************************************************************/
+#define RATE  0.5
+#define Kc    1.0
+#define Ti    0.001
+#define Td    0.0
+
+PID controllerA(Kc, Ti, Td, RATE);
+PID controllerB(Kc, Ti, Td, RATE);
 //ADC pins
 AnalogIn tempReadA(p15);
 AnalogIn tempReadB(p16);
 // DAC pin18
 //AnalogOut aout(p18);
 
-Ticker flipper;
-Ticker flipper2;
-PwmOut led1(LED1);
+//Ticker flipper;
+//Ticker flipper2;
+//PwmOut led1(LED1);
 //DigitalOut led1(LED1);
-DigitalOut led2(LED2);
+//DigitalOut led2(LED2);
 //DigitalOut led3(LED3);
 //DigitalOut led4(LED4);
 
@@ -23,48 +35,38 @@ SawTooth sawTooth(p18, 0.5);
 Flasher led3(LED3);
 Flasher led4(LED4, 2);
 
-void flip2() {
-  led1 = !led1;
-}
-
-void flip() {
-  led2 = !led2;
-}
-
 void readPC() {
   // Note: you need to actually read from the serial to clear the RX interrupt
-  //char _buffer[128];
   string holder;
   cJSON *json;
   // parameters list
-  // factor: scale of 3V
-  // ccles: number of periods to run
-  int period;
-  int cycles;
-  double factor;
+  double setPointA, setPointB, kc, ti, td;
 
   char temp;
   while(temp != '\n') {
     temp = pc.getc();
     holder += temp;
   }
+  if (holder.length() < 10) return;
+
   json = cJSON_Parse(holder.c_str());
   if (!json) {
     printf("Error before: [%s]\n", cJSON_GetErrorPtr());
   } else {
-    period = cJSON_GetObjectItem(json, "period")->valueint;
-    cycles = cJSON_GetObjectItem(json, "cycles")->valueint;
-    factor = cJSON_GetObjectItem(json, "factor")->valuedouble;
+    setPointA = cJSON_GetObjectItem(json, "setPointA")->valuedouble;
+    setPointB = cJSON_GetObjectItem(json, "setPointB")->valuedouble;
+    kc = cJSON_GetObjectItem(json, "kc")->valuedouble;
+    ti = cJSON_GetObjectItem(json, "ti")->valuedouble;
+    td = cJSON_GetObjectItem(json, "td")->valuedouble;
     cJSON_Delete(json);
   }
 
+  controllerA.setSetPoint(setPointA);
+  controllerA.setTunings(kc, ti, td);
+  controllerB.setSetPoint(setPointB);
+  controllerB.setTunings(kc, ti, td);
+  printf("setPoints: %3.1f'C %f3.1'C\n", setPointA, setPointB);
   printf("%s\n", holder.c_str());
-  printf("period is %d ms\n", period);
-  led1.period_ms(period);
-  led1.write(0.5f);
-  sawTooth.setWave(factor);
-  sawTooth.waveOut(cycles);
-  //led1 = !led1;
 }
 
 // Function to convert ADC reading to actual temp reading
@@ -74,20 +76,36 @@ double readRTD(double x) {
 }
 
 int main() {
-  led2 = 1;
+  double tempA, tempB;
+  //double read_bufferA[10] = {0,0,0,0,0,0,0,0,0,0};
+  //double read_bufferB[10] = {0,0,0,0,0,0,0,0,0,0};
+  double sumA = 0, sumB = 0;
   pc.attach(&readPC);
-  flipper.attach(&flip, 1); // the address of the function to be attached (flip) and the interval (2 seconds)
-  //flipper2.attach(&flip2, 1);
-  // spin in a main loop. flipper will interrupt it to call flip
-  //sawTooth.waveOut(1);
+
+  // Init PIC controllers
+  controllerA.setInputLimits(0.0, 350.0);
+  controllerA.setOutputLimits(0.0, 1.0);
+  controllerA.setSetPoint(20);
+  //controllerA.setBias(0.0);
+  controllerA.setMode(1);
+
   while(1) {
-    //led3.flash(1);
-    //led4.flash(3);
-    //pc.printf("testing\n");
-    //wait(1.0f);
     // print the temperatures
-    printf("Tube Sealer Temperature A: %3.3f'C\n", readRTD(tempReadA.read()));
-    printf("Tube Sealer Temperature B: %3.3f'C\n", readRTD(tempReadB.read()));
-    wait(1.0f);
+    // Read 10 times then average
+    sumA = 0;
+    sumB = 0;
+    for (int i=0; i<10; i++) {
+      sumA += tempReadA.read();
+      sumB += tempReadB.read();
+    }
+    tempA = readRTD(sumA/10);
+    tempB = readRTD(sumB/10);
+    //printf("Tube Sealer Temperature A: %3.4f'C\n", temp*3.3);
+    //printf("normalized: 0x%04X \n", tempReadA.read_u16());
+    controllerA.setProcessValue(tempA);
+    printf("Tube Sealer Temperature A: %3.1f'C\n", tempA);
+    printf("Compute PWM %3.3f\n", controllerA.compute());
+    //printf("Tube Sealer Temperature B: %3.1f'C\n", readRTD(tempB));
+    wait(RATE);
   }
 }

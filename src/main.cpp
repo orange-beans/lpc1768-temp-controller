de2bf685f17f5d3f0f06a571d151d6da75221db7
orange-beans <cczhang@simtech.a-star.edu.sg>
2018-01-17 15:30:06 +0800

    mails for inter thread COM;

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 #include "mbed_events.h"
+#include "main.h"
 #include <string>
 #include <cJSON.h>
 #include <Flasher.h>
@@ -14,8 +15,8 @@ DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 
 //ADC pins
-AnalogIn tempReadA(p15);
-AnalogIn tempReadB(p16);
+AnalogIn analogInA(p15);
+AnalogIn analogInB(p16);
 
 //PWM pins
 PwmOut heaterA(p21);
@@ -28,34 +29,54 @@ Serial dev(p28, p27, 115200);
 //****** Define Threads ******//
 // Define threads
 Thread realtimeThread(osPriorityRealtime, OS_STACK_SIZE, NULL, NULL);
-Thread operateThread(osPriorityHigh, OS_STACK_SIZE, NULL, NULL);
+//Thread operateThread(osPriorityAboveNormal, MEDIUM_STACK_SIZE, NULL, NULL);
+Thread displayTread(osPriorityBelowNormal, MEDIUM_STACK_SIZE, NULL, NULL);
 Thread commandThread(osPriorityNormal, OS_STACK_SIZE, NULL, NULL);
 
 // Define threads functions
-void RealtimeHandle();
-void LEDThread(void *param);
-void RealtimeTick();
+void realtimeHandle();
+void commandHandle();
+void displayHandle();
+
+void realtimeTick();
+//****** Define ISR ******//
+void commandISR();
+
+//****** Define Mails ******//
+typedef struct {
+  float   temperature;
+  float   pwm;
+} mail_t;
+
+typedef struct {
+  int cmdStr;
+} cmd_t;
+
+Mail<mail_t, 4> mail_box;
+Mail<cmd_t, 4> cmd_box;
 
 //****** Define Events ******//
 EventFlags event;
-
 
 //****** Define Queues ******//
 //EventQueue queue(32 * EVENTS_EVENT_SIZE);
 
 //****** Main ******//
 int main() {
+  // ISR handlers
+  pc.attach(&commandISR);
 
   // Create a queue with the default size
   EventQueue queue;
-  //EventQueue queue;
-  queue.call_in(2000, printf, "called in 2 seconds\n");
-  queue.call_every(1000, RealtimeTick);
+  //queue.call_in(2000, printf, "called in 2 seconds\n");
+  queue.call_every(INTERVAL_500MS, realtimeTick);
   //queue.call_every(1000, blink, "called every 1 seconds\n\r");
 
-  //thread.start(LEDThread);
-  realtimeThread.start(RealtimeHandle);
-  commandThread.start(callback(LEDThread, &led1));
+  // Start Threads
+  realtimeThread.start(realtimeHandle);
+  //displayTread.start(callback(displayHandle, &led1));
+  commandThread.start(commandHandle);
+  displayTread.start(displayHandle);
 
   // events are executed by the dispatch method
   queue.dispatch();
@@ -65,23 +86,89 @@ int main() {
 
 
 //****** Threads Callbacks ******//
-void RealtimeHandle() {
+void realtimeHandle() {
+  double temperature = 0;
+  double pwm = 0;
+  mail_t *sent_mail;
+
   while(true) {
-    event.wait_all(0x01);
+    // 1.Wait until timer tick
+    event.wait_all(REALTIME_TICK_S);
 
-    printf("Start executing Realtime Staffs!\r\n");
+    // 2.Read temperature
+    temperature = analogInA.read();
+
+    // 3.Calculate PWM
+    pwm = 10;
+    printf("executing Realtime Staffs!\r\n");
+
+    // 4.Send mail
+    sent_mail = mail_box.alloc();
+    sent_mail->temperature = temperature;
+    sent_mail->pwm = pwm;
+    mail_box.put(sent_mail);
   }
 }
 
-void LEDThread(void *param) {
-  while (true) {
+void displayHandle() {
+  osEvent evt;
+  mail_t *received_mail;
+  while(true) {
+    // Wait for mail to be avaliable;
+    evt = mail_box.get();
+    // Read mail
+    if (evt.status = osEventMail) {
+      received_mail = (mail_t*)evt.value.p;
+      printf("analog read is: %3.1f\r\n", received_mail->temperature);
+      printf("pwm setting is: %3.1f\r\n", received_mail->pwm);
+      // Free memory
+      mail_box.free(received_mail);
+    }
+
     led2 = !led2;
-    //led1 = !led1;
-    Thread::wait(1000);
+    //Thread::wait(1000);
   }
 }
 
-void RealtimeTick() {
+void commandHandle() {
+  osEvent evt;
+  cmd_t *received_cmd;
+  //string cmdStr;
+  int cmd;
+  while(true) {
+    //event.wait_all(COMMAND_S);
+
+    evt = cmd_box.get();
+    if (evt.status = osEventMail) {
+      received_cmd = (cmd_t*)evt.value.p;
+      cmd = received_cmd->cmdStr;
+      cmd_box.free(received_cmd);
+
+      printf("%d\n", cmd);
+    }
+  }
+}
+
+void realtimeTick() {
   led1 = !led1;
-  event.set(0x01);
+  event.set(REALTIME_TICK_S);
+}
+
+
+//****** ISR handles ******//
+void commandISR() {
+  //event.set(COMMAND_S);
+  string holder;
+  char temp;
+
+  while(temp != '\n') {
+    temp = pc.getc();
+    holder += temp;
+  }
+
+  printf("%s\n", holder.c_str());
+
+  cmd_t *sent_cmd = cmd_box.alloc();
+  sent_cmd->cmdStr = 666;
+  cmd_box.put(sent_cmd);
 }
---
+++ src/main.h
@@ -0,0 +1,18 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+//****** Define Constants ******//
+#define REALTIME_TICK_S     0x01
+#define COMMAND_S           0x02
+
+#define LARGE_STACK_SIZE    4096
+#define MEDIUM_STACK_SIZE   2048
+#define SMALL_STACK_SIZE    1024
+#define MINI_SMALL_SIZE     512
+
+#define INTERVAL_1000MS   1000
+#define INTERVAL_500MS    500
+#define INTERVAL_200MS    200
+#define INTERVAL_100MS    100
+
+#endif /* __MAIN_H */
